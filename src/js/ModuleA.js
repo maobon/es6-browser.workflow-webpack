1708a671a5bc32fc66b4706a9f85c324465bd9af
maobon <maobonhaha@gmail.com>
2017-05-15 14:49:53 +0800

    build

---
+++ src/js/ModuleA.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xinbob on 5/15/17.
+ */
+
+export const name = 'xinyi';
+export const age = 28;
+
+export function helloES6() {
+    console.log('Hello ES6')
+}
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xinbob on 5/15/17.
+ */
+
+import * as ModuleA from "./ModuleA";
+import {age, name} from "./ModuleA";
+
+console.log(ModuleA.name);
+
+console.log(name)
+console.log(age)
